ddc16444bc6ac4e80a84b27e856ae53c53e1cca6
johnnybolony <ctexyg@gmail.com>
2019-12-25 10:27:11 +0200

    Updates

---
+++ precache-manifest.5d7538d1f22dd7581490ab7e641d61dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71d68e592c015a8b097441555b48a80",
+    "url": "/react-cv-johnny/index.html"
+  },
+  {
+    "revision": "f8008837148c081b2b0e",
+    "url": "/react-cv-johnny/static/css/main.db7dbfe2.chunk.css"
+  },
+  {
+    "revision": "50fd680faef59e0c5d31",
+    "url": "/react-cv-johnny/static/js/2.650844d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-cv-johnny/static/js/2.650844d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8008837148c081b2b0e",
+    "url": "/react-cv-johnny/static/js/main.a830ff99.chunk.js"
+  },
+  {
+    "revision": "1b8f3f766ea530f7cd67",
+    "url": "/react-cv-johnny/static/js/runtime-main.24a70a04.js"
+  },
+  {
+    "revision": "34b26d19e8f55535ddd5bb03536f773f",
+    "url": "/react-cv-johnny/static/media/Capture.34b26d19.PNG"
+  },
+  {
+    "revision": "5bb3e2fe4693251b7f16de98315d2e04",
+    "url": "/react-cv-johnny/static/media/bananaleaves.5bb3e2fe.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-cv-johnny/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "bd30526235bbb70a656a30878fef9fd9",
+    "url": "/react-cv-johnny/static/media/memorygame.bd305262.PNG"
+  },
+  {
+    "revision": "3dff49215d5a5212caecc456cc32c233",
+    "url": "/react-cv-johnny/static/media/rimon.3dff4921.jpg"
+  },
+  {
+    "revision": "d6a8f90d5039ec88a2926cacc909c337",
+    "url": "/react-cv-johnny/static/media/swrthmr.d6a8f90d.jpg"
+  },
+  {
+    "revision": "3a7fc8d992bde3ad8a117bf97ab262b8",
+    "url": "/react-cv-johnny/static/media/todoapp.3a7fc8d9.png"
+  }
+]);
